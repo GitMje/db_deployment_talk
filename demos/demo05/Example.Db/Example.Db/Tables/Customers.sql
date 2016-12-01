df6cd5a434188320e9964e31a82157aa861de504
Michael Erpenbeck <michael.erpenbeck@market6.com>
2016-12-01 01:02:55 -0600

    Added sqlpackage and msbuild example

---
+++ demos/demo05/Example.Db/Example.Db/Tables/Customers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerId] INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]  NVARCHAR (50) NOT NULL,
+    [LastName]   NVARCHAR (50) NOT NULL,
+    [Age]        INT           NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([CustomerId] ASC)
+);
+
