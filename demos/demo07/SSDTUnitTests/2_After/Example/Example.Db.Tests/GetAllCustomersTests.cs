86cdb2622bd0bb1991a0ced9dccd458da2081235
Michael Erpenbeck <michael.erpenbeck@market6.com>
2016-12-08 13:28:01 -0600

    Added SSDT unit test example with before and after demos

---
+++ demos/demo07/SSDTUnitTests/2_After/Example/Example.Db.Tests/GetAllCustomersTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Db.Tests
+{
+    [TestClass()]
+    public class GetAllCustomersTests : SqlDatabaseTestClass
+    {
+
+        public GetAllCustomersTests()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_GetAllCustomersTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GetAllCustomersTests));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition rowCountCondition1;
+            this.dbo_GetAllCustomersTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_GetAllCustomersTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            rowCountCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition();
+            // 
+            // dbo_GetAllCustomersTestData
+            // 
+            this.dbo_GetAllCustomersTestData.PosttestAction = null;
+            this.dbo_GetAllCustomersTestData.PretestAction = null;
+            this.dbo_GetAllCustomersTestData.TestAction = dbo_GetAllCustomersTest_TestAction;
+            // 
+            // dbo_GetAllCustomersTest_TestAction
+            // 
+            dbo_GetAllCustomersTest_TestAction.Conditions.Add(rowCountCondition1);
+            resources.ApplyResources(dbo_GetAllCustomersTest_TestAction, "dbo_GetAllCustomersTest_TestAction");
+            // 
+            // rowCountCondition1
+            // 
+            rowCountCondition1.Enabled = true;
+            rowCountCondition1.Name = "rowCountCondition1";
+            rowCountCondition1.ResultSet = 1;
+            rowCountCondition1.RowCount = 3;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_GetAllCustomersTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_GetAllCustomersTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions dbo_GetAllCustomersTestData;
+    }
+}
---
+++ demos/demo07/SSDTUnitTests/2_After/Example/Example.Db/Stored Procedures/GetAllCustomers.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetAllCustomers]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT [CustomerId], [FirstName], [LastName], [Age]
+	FROM dbo.[Customers]
+END
